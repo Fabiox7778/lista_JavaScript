d3005c19394e50a660c1feac5d44cda374073c47
Fabiox7778 <fabiorabello08@gmail.com>
2025-03-28 16:25:25 -0300

    feat(operadores-aritmeticos): adiciona divisão de valor entre 4 pessoas

---
+++ Operadores_Aritmeticos/divisao_grana.js
@@ -0,0 +1,6 @@
+let dinheiro = 200
+let pessoas = 4 
+
+let DivPorPessoa = dinheiro / pessoas
+
+console.log(`O resultado é ${DivPorPessoa}`)
--- Operadores_Aritmeticos/divisão_de_grana.js
+++
@@ -1,6 +0,0 @@
-let dinheiro = 200
-let pessoas = 4
-
-let divPorPessoas = dinheiro/pessoas
-
-console.log(divPorPessoas)
