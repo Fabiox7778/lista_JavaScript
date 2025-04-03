939fbf4cf7d9066bb49637a62b295c2388ec7727
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 16:28:16 -0300

    adiciona verificação de aprovação por nota

---
+++ If_Else/Aprovado_ou_Não.js
@@ -0,0 +1,11 @@
+nota = 80;
+
+if (nota >= 0 && nota <= 100) {
+    if (nota >= 60) {
+        console.log("Aprovado");
+    } else {
+        console.log("Reprovado");
+    }
+} else {
+    console.log("Nota invalida");
+}
