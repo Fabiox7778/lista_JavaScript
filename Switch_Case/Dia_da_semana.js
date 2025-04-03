cfc484f7d8a5f22ac820d6f901c3fd8402f71bb1
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 17:44:15 -0300

    adiciona exibição do dia da semana

---
+++ Switch_Case/Dia_da_semana.js
@@ -0,0 +1,25 @@
+diaDaSemana = 6
+
+switch (diaDaSemana) {
+    case 1:
+        console.log("hoje é Domimgo");
+        break;
+    case 2:
+        console.log("Hoje é Segunda-Feira");
+        break;
+    case 3:
+        console.log("Hole é Terça-Feira");
+        break;
+    case 4:
+        console.log("Hoje é Quarta-feira");
+        break;
+    case 5:
+        console.log("Hoje é Quinta-feira");
+        break;
+    case 6:
+        console.log("Hoje é Sexta-Feira");
+        break;
+    case 7:
+        console.log("Hoje é Sabado");
+        break;
+}
