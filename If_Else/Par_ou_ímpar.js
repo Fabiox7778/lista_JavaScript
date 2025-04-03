5fb51cd99152f2303201aacdf5c68ec5f636bc3d
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 16:50:16 -0300

    commit

--- If_Else/Par_ou_ímpar.js
+++ If_Else/Par_ou_ímpar.js
@@ -1,7 +1,7 @@
-num = 22
+num = 22;
 
 if (num % 2 == 0) {
-    console.log(`${num} é par`)
+    console.log(`${num} é par`);
 } else {
-    console.log(`${num} é impar`)
+    console.log(`${num} é impar`);
 }
