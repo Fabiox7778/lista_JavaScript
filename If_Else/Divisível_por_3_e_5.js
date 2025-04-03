e3174316b41d306e0bad640e794f4c4ffe5ea48b
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 16:51:27 -0300

    adiciona verificação de divisibilidade por 3 e 5

--- If_Else/Divisível_por_3_e_5.js
+++ If_Else/Divisível_por_3_e_5.js
@@ -1,7 +1,7 @@
 num = 90;
 
 if (num % 3 == 0 && num % 5 == 0) {
-    console.log(`${num} é divisivel por 3 e 5`);
+    console.log(`${num} é divisivel pelo 3 e o 5`);
 } else {
     console.log(`${num} não é divisivel por 3 e 5`);
 }
