23fc7d43b7b69979837f81d98a1b284926bcbad5
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 15:19:28 -0300

    adiciona verificação de número maior que 100

---
+++ Operadores_Lógicos/maior_que_100.js
@@ -0,0 +1,9 @@
+num1 = 71;
+num1 = 94;
+num1 = 156;
+
+if (num1 > 100 || num2 > 100 || num3 > 100 ) {
+    console.log("Pelo menos um dos números é maior que 100");
+}  else {
+    console.log("nenhum dos numeros é maior que 100");
+}
