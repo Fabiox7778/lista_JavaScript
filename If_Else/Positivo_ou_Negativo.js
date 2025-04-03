5856d548f064c27a474e2d2a8bec167a98e5ea14
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 15:52:48 -0300

    adiciona verificação de positivo, negativo ou zero

---
+++ If_Else/Positivo_ou_Negativo.js
@@ -0,0 +1,9 @@
+num = 22;
+
+if (num > 0) {
+    console.log(`${num} é positivo`);
+} else if (num < 0) {
+    console.log(`${num} é negativo`);
+} else {
+    console.log("Esse numero é igual a 0");
+}
