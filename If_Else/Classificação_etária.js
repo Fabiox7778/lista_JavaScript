5692ff8424792f979d956f9658002e96b6dd07f6
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 16:04:41 -0300

    adiciona classificação etária por idade

---
+++ If_Else/Classificação_etária.js
@@ -0,0 +1,11 @@
+idade = 15;
+
+if (idade > 60) {
+    console.log("idoso");
+} else if (idade > 18) {
+    console.log("adulto");
+} else if (idade > 12) {
+    console.log("adolecente");
+} else {
+    console.log("criança");
+}
