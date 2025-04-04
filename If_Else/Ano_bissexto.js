c53fdcbcbba47ed46f4fe2e0822ff9cf153f6ada
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-04-04 08:15:38 -0300

    adiciona verificação de ano bissexto

--- If_Else/Ano_bissexto.js
+++ If_Else/Ano_bissexto.js
@@ -1,6 +1,6 @@
-ano = 2024
+let ano = 2024
 
-if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0 ) {
+if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
     console.log(`${ano} é um ano bisexto`)
 } else {
     console.log(`${ano} não é um ano bissexto`)
