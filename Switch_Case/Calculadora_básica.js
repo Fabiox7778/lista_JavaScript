320a241e5e24158edc7aced9c86a22f44e7eead8
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 23:23:28 -0300

    adiciona calculadora básica com operações

---
+++ Switch_Case/Calculadora_básica.js
@@ -0,0 +1,22 @@
+num1 = 30;
+num2 = 10;
+
+operacao = 1;
+
+switch (operacao) {
+    case 1:
+        console.log("soma = " + (num1+num2));
+        break;
+    case 2:
+        console.log("Subtração = " + (num1-num2));
+        break;
+    case 3:
+        console.log("Multiplicação = " + (num1*num2));
+        break;
+    case 4:
+        console.log("Divisão = " + (num1+num2));
+        break;
+    default:
+        console.log("Operacação invalida");
+        break;
+}
