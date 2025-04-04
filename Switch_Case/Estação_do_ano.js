2b280da18b46cd576f97e9679280b27d63b6bbbe
Fabio Henrique <fabiotrevizolli8@gmail.com>
2025-04-03 23:50:38 -0300

    adiciona exibição da estação do ano

---
+++ Switch_Case/Estação_do_ano.js
@@ -0,0 +1,19 @@
+let estação = 2;
+
+switch (estação) {
+    case 1:
+        console.log("É verão");
+        break;
+    case 2:
+        console.log("É outono");
+        break;
+    case 3:
+        console.log("É inverno");
+        break;
+    case 4:
+        console.log("É primavera");
+        break;
+    default:
+        console.log("Estação invalida");
+        break;
+}
